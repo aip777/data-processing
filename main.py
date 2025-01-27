9329f99ac8d1843c40eeef8f544fe542acbd4e93
Palash <ariful.islamk777@gmail.com>
2025-01-28 00:13:33 +0600

    images download

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ def download_image(save_path):
                     image_url = image_url.strip()
                     image_name = os.path.basename(image_url)
                     save_path = os.path.join(output_folder, image_name)
-                response = requests.get(image_url, stream=True, timeout=30)
-                if response.status_code == 200:
-                    with open(save_path, 'wb') as f:
-                        for chunk in response.iter_content(1024):
-                            f.write(chunk)
-                    print(f"Downloaded: {save_path}")
-                else:
-                    print(f"Failed to download {image_url} - Status code: {response.status_code}")
+                    response = requests.get(image_url, stream=True, timeout=30)
+                    if response.status_code == 200:
+                        with open(save_path, 'wb') as f:
+                            for chunk in response.iter_content(1024):
+                                f.write(chunk)
+                        print(f"Downloaded: {save_path}")
+                    else:
+                        print(f"Failed to download {image_url} - Status code: {response.status_code}")
     except Exception as e:
         print(f"Error downloading {e}")
 download_image(output_folder)
